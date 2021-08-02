9548e7e49a86938943c4142db485f50f5c97b300
TBoileau <t-boileau@email.com>
2021-08-02 11:52:42 +0200

    feat: handle form (#36)

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -6,14 +6,32 @@ namespace TBoileau\Oc\Php\Project5\Controller;
 
 use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use TBoileau\Oc\Php\Project5\DependencyInjection\ServicesSubscriberInterface;
+use TBoileau\Oc\Php\Project5\Form\FormFactoryInterface;
 use TBoileau\Oc\Php\Project5\Router\RouterInterface;
 use TBoileau\Oc\Php\Project5\Templating\TemplatingInterface;
 
 abstract class AbstractController implements ServicesSubscriberInterface
 {
     private ContainerInterface $container;
+
+    public function handleForm(string $formName, object $data, Request $request, callable $callable): bool
+    {
+        /** @var FormFactoryInterface $formFactory */
+        $formFactory = $this->container->get(FormFactoryInterface::class);
+
+        $form = $formFactory->create($formName, $data)->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            call_user_func($callable, [$data]);
+
+            return true;
+        }
+
+        return false;
+    }
 
     /**
      * @param array<string, mixed> $context
@@ -50,6 +68,7 @@ abstract class AbstractController implements ServicesSubscriberInterface
         return [
             RouterInterface::class,
             TemplatingInterface::class,
+            FormFactoryInterface::class,
         ];
     }
 }
